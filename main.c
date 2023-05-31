8b0599e07fa756dcf69a0e258d115ad4db37bf22
João Pedro Batista da silva <116274892+Joaopedrobs7@users.noreply.github.com>
2023-05-30 23:53:30 -0300

    initial feedback test
    tudo bugado

--- main.c
+++ main.c
@@ -4,10 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "auth.h"
-
+//int residente1=1, residente2=2, residente3=3;
+//char f_resident1[150], f_resident2[150], f_resident3[150];
+//int option;
 int main() {
     initializeDatabase();
-    int choice;
+    int choice,choice_feedback;
     char email[20];
     char password[20];
 
@@ -51,10 +53,36 @@ int main() {
                 printf("Invalid choice. Please try again.\n");
                 break;
         }
-        printf("\n");
+//        //prototipo registro de feedback
+//        if (loggedInUser != NULL){
+//            printf("1. Registrar feedback\n");
+//            printf("2. Exit\n");
+//            scanf("%d",&option);
+//            switch (option) {
+//                case 1 : {
+//                    scanf("%d", &choice_feedback);
+//
+//                    switch (choice_feedback) {
+//                        case 1:
+//                            printf("Feedback residente Numero: %d\n", residente1);
+//                            fgets(f_resident1, 150, stdin);
+//                            break;
+//                        case 2:
+//                            printf("Feedback residente Numero: %d\n", residente2);
+//                            fgets(f_resident2, 150, stdin);
+//                            break;
+//                        case 3:
+//                            printf("Feedback residente Numero: %d\n", residente3);
+//                            fgets(f_resident3, 150, stdin);
+//                            break;
+//                    }
+//                    break;
+//                }
+//                case 2: {
+//                    exit(1);}
+//            }
+//        }
     }
-
-    return 0;
-//    printf("%u", hashPassword("admin"));
+     //printf("%u", hashPassword("admin"));
     return 0;
 }
